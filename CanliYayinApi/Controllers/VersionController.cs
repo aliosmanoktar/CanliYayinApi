28df5f5c649b5f081e8f3e159b49bc77686c42a0
Ali Osman OKTAR <aliosmanoktar@gmail.com>
2019-08-22 18:27:05 +0300

    Version Control Added

---
+++ CanliYayinApi/Controllers/VersionController.cs
@@ -0,0 +1,23 @@
+﻿using CanliYayinApi.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace CanliYayinApi.Controllers
+{
+    public class VersionController : ApiController
+    {
+        
+        public Version Get()
+        {
+            return new Version
+            {
+                version = "2.1"
+            };
+        }
+    }
+}
---
+++ CanliYayinApi/Models/Version.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CanliYayinApi.Models
+{
+    public class Version
+    {
+        public string version { set; get; }
+        public string path {
+            get {
+                return $"{HttpContext.Current.Request.Url.Authority}/app_assets/apk/app-{version.Replace('.','_')}.apk";
+            }
+        }
+    }
+}
